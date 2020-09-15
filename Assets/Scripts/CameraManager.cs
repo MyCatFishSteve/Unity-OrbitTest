00c76fea5a98776798019b24e9854ebcff4c9e9b
James Allenby <JamesAllenby@protonmail.com>
2020-09-15 12:23:44 +0100

    Initial commit

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private bool m_ZoomActive;
+    private GameObject m_Player;
+    private Camera m_Camera;
+
+    private float velocity;
+    
+    [SerializeField] private float m_DefaultZoom = 5f;
+    [SerializeField] private float m_ActiveZoom = 7f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("Player");
+        m_Camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float selectedZoom = Input.GetKey(KeyCode.Space) ? m_ActiveZoom : m_DefaultZoom;
+        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, selectedZoom, ref velocity, 0.1f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float m_MovementSpeed = 5.0f;
+    
+    private float m_MovementDirection;
+
+    private Rigidbody2D m_Rigidbody2D;
+
+    private GameObject m_Planet;
+
+#if UNITY_EDITOR
+    [SerializeField] private bool m_DebugMode = false;
+#endif
+
+    private void Start()
+    {
+        m_Rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        m_MovementDirection = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+#if UNITY_EDITOR
+        if (m_DebugMode)
+        {
+            Debug.DrawRay(transform.position, Vector2.zero - (Vector2)transform.position);
+        }
+#endif
+        Vector2 planetNormal = Vector2.zero;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.zero - (Vector2)transform.position);
+        if (hit.collider)
+        {
+            planetNormal = hit.normal;
+            float desiredAngle = Vector2.SignedAngle(Vector2.up, hit.normal);
+            transform.eulerAngles = new Vector3(0, 0, desiredAngle);
+#if UNITY_EDITOR
+            if (m_DebugMode)
+            {
+                Debug.DrawRay(hit.point, hit.normal, Color.yellow);
+                Debug.DrawRay(hit.point, Vector2.Perpendicular(hit.normal), Color.blue);
+            }
+#endif
+            m_Rigidbody2D.AddForce(-hit.normal * 9.81f);
+            
+        }
+
+        m_Rigidbody2D.velocity += -Vector2.Perpendicular(planetNormal) * m_MovementDirection * m_MovementSpeed;
+    }
+}
